1dcda1335dae1bfa39cbde63114a4d39b1524b6d
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-07-14 16:17:48 -0400

    image a day should print errors now

--- scripts/image_a_day.py
+++ scripts/image_a_day.py
@@ -1,25 +1,30 @@
 import json
 import requests
+import traceback
 import urllib.request
 from datetime import datetime
 
-asBG = "/media/Library/SPEwww/find-it/img/background.jpg"
-descFile = "/media/Library/SPEwww/find-it/bgDesc.json"
+try:
+	asBG = "/media/Library/SPEwww/find-it/img/background.jpg"
+	descFile = "/media/Library/SPEwww/find-it/bgDesc.json"
 
-print (f"Started at {datetime.now()}\n")
+	print (f"Started at {datetime.now()}\n")
 
-r = requests.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8')
+	r = requests.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8')
 
-imageData = r.json()["images"][0]
-url = "https://www.bing.com" + imageData["url"]
-desc = imageData["copyright"]
-title = imageData["title"]
-if "(" in desc:
-	desc = desc.split("(")[0].strip()
-moreDetails = imageData["copyrightlink"]
+	imageData = r.json()["images"][0]
+	url = "https://www.bing.com" + imageData["url"]
+	desc = imageData["copyright"]
+	title = imageData["title"]
+	if "(" in desc:
+		desc = desc.split("(")[0].strip()
+	moreDetails = imageData["copyrightlink"]
 
-urllib.request.urlretrieve(url, asBG)
+	urllib.request.urlretrieve(url, asBG)
 
-data = {"description": desc, "link": moreDetails, "title": title}
-with open(descFile, 'w') as outfile:
-    json.dump(data, outfile)
+	data = {"description": desc, "link": moreDetails, "title": title}
+	with open(descFile, 'w') as outfile:
+	    json.dump(data, outfile)
+
+except Exception as e:
+    print(f"Error at {datetime.now()}:\n{traceback.format_exc()}\n")
