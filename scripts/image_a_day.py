d97740194e3096ad693988d1cfb2e81b2aa1a126
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-07-14 16:23:18 -0400

    more debugging statements

--- scripts/image_a_day.py
+++ scripts/image_a_day.py
@@ -8,9 +8,10 @@ try:
 	asBG = "/media/Library/SPEwww/find-it/img/background.jpg"
 	descFile = "/media/Library/SPEwww/find-it/bgDesc.json"
 
-	print (f"Started at {datetime.now()}\n")
+	print (f"Started at {datetime.now()}")
 
 	r = requests.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8')
+	print (f"\t--> {r.status_code}")
 
 	imageData = r.json()["images"][0]
 	url = "https://www.bing.com" + imageData["url"]
@@ -23,6 +24,7 @@ try:
 	urllib.request.urlretrieve(url, asBG)
 
 	data = {"description": desc, "link": moreDetails, "title": title}
+	print (f"\tWriting to {descFile}...")
 	with open(descFile, 'w') as outfile:
 	    json.dump(data, outfile)
 
