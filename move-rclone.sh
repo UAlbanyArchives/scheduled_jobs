8381a1fd002d63753bdd97edb5c16f335e4764c4
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-12-17 10:53:25 -0500

    detached mode won't work

--- move-rclone.sh
+++ move-rclone.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# usage: move-rclone <folder>
 folder=$1
 
 if [ -z "$folder" ]; then
@@ -8,15 +7,17 @@ if [ -z "$folder" ]; then
   exit 1
 fi
 
-docker compose -f ~/scheduled_jobs/docker-compose.yml run -d jobs rclone \
-  copy /media/Masters/Archives/AIP/$folder b2:$folder \
+docker compose -f ~/scheduled_jobs/docker-compose.yml run --rm jobs bash -c "\
+rclone copy /media/Masters/Archives/AIP/$folder b2:$folder \
   --transfers 16 \
   --checkers 32 \
   --fast-list \
-  --log-file /logs/rclone/inital-$folder.log \
+  --log-file /logs/rclone/rclone-initial-$folder.log \
   --log-level INFO \
   --stats 30s \
   --retries 3 \
-  --low-level-retries 10
+  --low-level-retries 10 & \
+wait \$! \
+"
 
-echo "Started rclone upload for $folder (logs: SPE_Automated/rclone/inital-$folder.log)"
+echo "Started rclone upload for $folder (logs: SPE_Automated/rclone/rclone-initial-$folder.log)"
