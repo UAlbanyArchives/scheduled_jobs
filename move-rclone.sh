f977102328c34661002fb6012f820b36ce358238
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-12-17 09:52:39 -0500

    added copy script

---
+++ move-rclone.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# usage: move-rclone <folder>
+folder=$1
+
+if [ -z "$folder" ]; then
+  echo "Usage: move-rclone <folder>"
+  exit 1
+fi
+
+docker compose -f ~/scheduled_jobs/docker-compose.yml run -d rclone \
+  copy /media/Masters/Archives/AIP/$folder b2:/$folder \
+  --transfers 16 \
+  --checkers 32 \
+  --fast-list \
+  --log-file /opt/rclone/inital-$folder.log \
+  --log-level INFO \
+  --stats 30s \
+  --retries 3 \
+  --low-level-retries 10
+
+echo "Started rclone upload for $folder (logs: /opt/rclone/inital-$folder.log)"
