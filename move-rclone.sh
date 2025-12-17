5b675e4fa0d3a551057f5315243cd9127c431a71
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-12-17 11:06:23 -0500

    trying this way

--- move-rclone.sh
+++ move-rclone.sh
@@ -7,17 +7,15 @@ if [ -z "$folder" ]; then
   exit 1
 fi
 
-docker compose -f ~/scheduled_jobs/docker-compose.yml run --rm jobs bash -c "\
-rclone copy /media/Masters/Archives/AIP/$folder b2:/$folder \
+docker compose -f ~/scheduled_jobs/docker-compose.yml run --rm jobs \
+  rclone copy "/media/Masters/Archives/AIP/$folder" "b2:$folder" \
   --transfers 16 \
   --checkers 32 \
   --fast-list \
-  --log-file /logs/rclone/rclone-initial-$folder.log \
+  --log-file "/logs/rclone-initial-$folder.log" \
   --log-level INFO \
   --stats 30s \
   --retries 3 \
-  --low-level-retries 10 & \
-wait \$! \
-"
+  --low-level-retries 10 &
 
 echo "Started rclone upload for $folder (logs: SPE_Automated/rclone/initial-$folder.log)"
